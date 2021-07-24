b99ad1bb2f04c321108252e9edd9ae6407b3ee08
HemanthMaram <83005267+HemanthMaram@users.noreply.github.com>
2021-07-24 23:27:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const url ="https://pokeapi.co/api/v2/pokemon/";
 
-var maindiv = createElement('div','main');
+var maindiv = createElement('div','container-sm main');
 var hpart = createElement('div','heading')
 
 var titlt = createElement('h2',' text-center mt-2');
